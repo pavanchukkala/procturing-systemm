f777797fe15234f97ed79eb5b83bdb9308e69aec
chukkala pavan kumar <150168684+pavanchukkala@users.noreply.github.com>
2025-05-13 11:25:53 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -17,6 +18,11 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  webpack(config) {
+    // Add this line to resolve '@' alias
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
   },
 };
 
